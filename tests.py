c4cb758f198960af09408b72220ac012983658ab
David Lord <eternalcat@gmail.com>
2017-10-22 12:30:39 +1000

    Profiling test uses the new system

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import scrape, config
 from bs4 import BeautifulSoup
 import requests
 from tempfile import TemporaryDirectory
-import os
+import asyncio, aiohttp
 
 
 class PerfTesting(unittest.TestCase):
@@ -17,20 +17,11 @@ class PerfTesting(unittest.TestCase):
         self.emoji_pairs = list(scrape.parse_emoji_from_html(soup))
         self.assertGreater(len(self.emoji_pairs), 0)  # I wish we could do this without consuming the generator :(
 
-    def testWithoutConnectionReuse(self):
+    def testProfile(self):
         with TemporaryDirectory() as dest:
-            start = time()
-            scrape.download_all_emoji(self.emoji_pairs[:self.__pairs_tested], dest, requests.get)
-            end = time()
-            count = len(os.listdir(dest))
-        print(f"Completed {count} files in {end - start} seconds.")
-
-    def testWithConnectionReuse(self):
-        session = requests.Session()
-        with TemporaryDirectory() as dest:
-            start = time()
-            scrape.download_all_emoji(self.emoji_pairs[:self.__pairs_tested], dest, session.get)
-            end = time()
-            count = len(os.listdir(dest))
-        print(f"Completed {count} files in {end - start} seconds.")
-        session.close()
+            with aiohttp.ClientSession() as session:
+                loop = asyncio.get_event_loop()
+                start = time()
+                loop.run_until_complete(scrape.download_all_emoji(self.emoji_pairs[:self.__pairs_tested], dest, session))
+                end = time()
+        print(f"Completed in {end - start} seconds.")
