680a79f77d3e6d97e4ed7e1e03a45fc84b1b1ee3
David Lord <eternalcat@gmail.com>
2017-10-23 11:39:31 +1000

    Reorder profiling graph
    It's more appropriate to align top-bottom with early-recent. Helps the
    reader understand, I think.
    
    I've also rewritten the data handling because dicts aren't reliably sorted.
    Suspect it's Python-3.6 specific that the keys and values are aligned,
    and that's not guaranteed by the language yet.

--- profiling-graph.py
+++ profiling-graph.py
@@ -6,10 +6,11 @@ Documenting an unscientific performance test (the content of 'tests.py').
 """
 
 results = {"sequential": 575.622, "connection reuse": 345.897, "aiohttp": 21.304}
+methods, times = zip(*sorted(results.items()))
+num_results = np.arange(len(methods))
 
-y_pos = np.arange(len(results))
-plt.barh(y_pos, [time for _, time in results.items()], align='center', alpha=0.5)
-plt.yticks(y_pos, [method for method, _ in results.items()])
+plt.barh(num_results, times, align='center', alpha=0.5)
+plt.yticks(num_results, methods)
 plt.xlabel("Time (s)")
 plt.title("Time to download 500 entries")
 
