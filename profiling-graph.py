9a0272a481dbadd7b2490ef54cd1788d4ff23530
David Lord <eternalcat@gmail.com>
2017-10-23 10:49:52 +1000

    More interesting magnitude if it's sideways

--- profiling-graph.py
+++ profiling-graph.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+"""
+Documenting an unscientific performance test (the content of 'tests.py').
+"""
+
 results = {"sequential": 575.622, "connection reuse": 345.897, "aiohttp": 21.304}
 
 y_pos = np.arange(len(results))
-plt.bar(y_pos, [time for _, time in results.items()], align='center', alpha=0.5)
-plt.xticks(y_pos, [method for method, _ in results.items()])
-plt.ylabel("Time (s)")
+plt.barh(y_pos, [time for _, time in results.items()], align='center', alpha=0.5)
+plt.yticks(y_pos, [method for method, _ in results.items()])
+plt.xlabel("Time (s)")
 plt.title("Time to download 500 entries")
 
 plt.show()
